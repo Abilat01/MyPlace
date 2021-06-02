b228c0893e28d29c0552f4a153063ca31af20466
Abilat01 <Abilat01@yandex.ru>
2021-06-02 17:13:32 +0300

    Главная иконка и лаунч скрин

--- MyMapsPlace/MainViewController.swift
+++ MyMapsPlace/MainViewController.swift
@@ -31,9 +31,18 @@ class MainViewController: UITableViewController {
         
         cell.textLabel?.text = restaurantNames[indexPath.row]
         cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
 
 
         return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+    //высота строки
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
     
 
